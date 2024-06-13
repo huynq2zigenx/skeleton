3f6f99d3da59b30e3793852ad1861ac2ce8d712c
Le Hoang Lan <lanlh@zigexn-vn>
2024-06-13 17:41:12 +0700

    change: Change pagination

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -13,23 +13,23 @@ defineProps({
 
 <template>
    <div class="join hidden sm:block">
-		<a v-if="model.prev_page_url" :href="model.prev_page_url" class="join-item btn mr-1 disabled:opacity-50 disabled:cursor-not-allowed bg-white">
+		<a :class="model.current_page == 1 ? 'pointer-events-none': ''" :href="model.prev_page_url" class="join-item btn mr-1 disabled:opacity-50 bg-white">
 			Previous
 		</a>
 		<template v-for="n in total" :key="n">
 			<a :href="`${model.path}?page=${n}`" class="rounded-md">
-				<span class="join-item btn bg-white" :class="model.current_page === n ? 'bg-blue-500 text-white' : ''">{{ n }}</span>
+				<span class="join-item btn" :class="model.current_page == n ? 'bg-blue-500 text-white' : 'bg-white'">{{ n }}</span>
 			</a>
 		</template>
-		<a v-if="model.current_page < total" :href="`${model.path}?page=${model.current_page + 1}`" class="join-item btn mr-1 disabled:opacity-50 disabled:cursor-not-allowed bg-white">
+		<a :class="model.current_page == total ? 'pointer-events-none': ''" :href="`${model.path}?page=${model.current_page + 1}`" class="join-item btn mr-1 disabled:opacity-50 bg-white">
 			Next
 		</a>
 	</div>
 	<div class="join grid grid-cols-2 sm:hidden ml-1">
-		<a v-if="model.prev_page_url" :href="model.prev_page_url" class="join-item btn btn-outline disabled:opacity-50 disabled:cursor-not-allowed">
+		<a :class="model.current_page == 1 ? 'pointer-events-none hover:bg-gray-300 bg-gray-300': ''" :href="`${model.path}?page=${model.current_page - 1}`" class="join-item btn bg-white">
 			Previous page
 		</a>
-		<a v-if="model.current_page < total" :href="`${model.path}?page=${model.current_page + 1}`" class="join-item btn btn-outline disabled:opacity-50 disabled:cursor-not-allowed">
+		<a  :class="model.current_page == total ? 'pointer-events-none hover:bg-gray-300 bg-gray-300': ''" :href="`${model.path}?page=${model.current_page + 1}`" class="join-item btn bg-white">
 			Next page
 		</a>
 	</div>
